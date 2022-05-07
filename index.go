cb53884d93df0d3fe3eff7583783983cbb627ba2
Teerut26 <teerutsr@gmail.com>
2022-05-07 09:06:00 +0700

    init commit

--- index.go
+++ index.go
@@ -1,57 +1,33 @@
 package main
 
 import (
-	"fmt"
-	"time"
+	"strings"
 )
 
-func calulate(x int, y int) int {
-	return x + y
+type Profile struct {
+	name string
+	age  int
 }
 
-func showArray(list_name []string) {
-	for i := 0; i < len(list_name); i++ {
-		fmt.Println("[", i, "] => ", list_name[i])
-	}
+func upperAllLetter(p *Profile) {
+	*&p.name = strings.ToUpper(*&p.name)
 }
 
-func showHelloWorld(n int, word string, delay int) {
-	for i := 0; i < n; i++ {
-		fmt.Println(word, " => ", i+1)
-		time.Sleep(time.Duration(delay) * time.Second)
-	}
-}
-
-func showPiramid(layer int) {
-	for i := 0; i < layer; i++ {
-		if i != 0 {
-			for j := layer - i; j > 1; j-- {
-				fmt.Print(" ")
-			}
-		}
-		for j := 0; j < i; j++ {
-			fmt.Print("*")
-		}
-		for j := 1; j < i; j++ {
-			fmt.Print("*")
-		}
-		fmt.Print("\n")
-	}
+func increaseAge(p *Profile, increaseValue int) {
+	*&p.age = p.age + increaseValue
 }
 
 func main() {
-	// n := 0
-	// word := "Hello World"
+	p := Profile{
+		name: "holope",
+		age:  18,
+	}
 
-	// list_name := []string{"b1", "b2"}
+	upperAllLetter(&p)
+	increaseAge(&p, 5)
 
-	// fmt.Print("hello word lines : ")
-	// fmt.Scan(&n)
-
-	// showHelloWorld(n, word, 1)
-
-	// showArray(list_name)
-
-	showPiramid(10)
-
+	println("name => ", p.name)
+	println("age => ", p.age)
 }
+
+// ref https://dev.to/iporsut/go-pointer-pointer-go-3212
